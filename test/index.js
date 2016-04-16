9508327136f527c4152cbbdd69ed958862f7c06c
Jason Miller <jason@developit.ca>
2016-04-16 14:54:48 -0400

    Test harness.

---
+++ test/index.js
@@ -0,0 +1,13 @@
+import { expect } from 'chai';
+import VirtualList from '../src';
+/**@jsx h */
+
+/*eslint-env mocha*/
+
+describe('preact-virtual-list', () => {
+	it('should export a class', () => {
+		expect(VirtualList).to.be.a('function');
+	});
+
+	xit('should have proper tests', () => {});
+});
